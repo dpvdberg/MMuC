021049927f988ebc2e32f2da92582ea7e5f6abb4
Sophie <svdeeren@hotmail.com>
2020-02-20 21:45:06 +0100

    improved algorithm

--- src/main/kotlin/ModalMu/ModalUtils.kt
+++ src/main/kotlin/ModalMu/ModalUtils.kt
@@ -1,5 +1,23 @@
 package ModalMu
 
+import javax.swing.tree.VariableHeightLayoutCache
+
 fun ModalFormula.getVariables() : List<Variable> {
     return emptyList()
+}
+
+fun ModalFormula.getFixedPointVariable(x : Variable) : FixedPoint {
+    return FixedPoint.MU
+}
+
+fun ModalFormula.getSurroundingFixedPoint(x : Variable) : FixedPoint {
+    return FixedPoint.MU
+}
+
+fun ModalFormula.getSubVariables() : List<Variable> {
+    return emptyList()
+}
+
+fun ModalFormula.isSentence() : Boolean {
+    return false
 }
